c11779d811536ad880f76dcf13d88bea409fa2b9
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-12-22 15:27:07 +0100

    Script Post-Deploiement + correction table Acting (PRIM KEY)

--- MovieCollectionDb/Tables/Acting.sql
+++ MovieCollectionDb/Tables/Acting.sql
@@ -2,7 +2,8 @@
 (
 	[Act_IdArtist] INT NOT NULL , 
     [Act_IdMovie] INT NOT NULL, 
-    PRIMARY KEY ([Act_IdArtist],[Act_IdMovie]), 
+    [Act_Character] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY ([Act_IdArtist],[Act_IdMovie],[Act_Character]), 
     CONSTRAINT [FK_Acting_Artist] FOREIGN KEY (Act_IdArtist) REFERENCES Artist, 
     CONSTRAINT [FK_Acting_Movie] FOREIGN KEY (Act_IdMovie) REFERENCES Movie
 )
