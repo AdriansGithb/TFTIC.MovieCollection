e8f267c81ab2993d477404d055c91961dc831d91
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-12-22 16:04:39 +0100

    Add Entitites to DAL

---
+++ MovieCollectionDAL/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollectionDAL.Entities
+{
+    public class Movie
+    {
+        public int IdMovie { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Synopsys { get; set; }
+        public string TrailerLink { get; set; }
+        public bool IsDeleted { get; set; }
+        public int IdCountry { get; set; }
+        public int IdAudience { get; set; }
+    }
+}
