dc96dffeba8debb4fe21ea34313f33be2efea710
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-12-22 16:30:11 +0100

    + ArtistService + IArtistRepository (+BaseService)

---
+++ MovieCollectionDAL/Repositories/IArtistRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieCollectionDAL.Entities;
+using System.Collections.Generic;
+
+namespace MovieCollectionDAL.Repositories
+{
+    public interface IArtistRepository
+    {
+        bool Create(Artist a);
+        bool Delete(int id);
+        IEnumerable<Artist> GetAll();
+        Artist GetById(int Id);
+        bool Update(Artist a);
+    }
+}
---
+++ MovieCollectionDAL/Services/ArtistService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using MovieCollectionDAL.Entities;
+using MovieCollectionDAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace MovieCollectionDAL.Services
+{
+    public class ArtistService : BaseService<Artist>,IArtistRepository
+    {
+
+        public ArtistService(IConfiguration config) : base("Artist", config.GetConnectionString("default"))
+        {
+        }
+
+        internal override Artist Converter(IDataRecord reader)
+        {
+            return new Artist
+            {
+                IdArtist = (int)reader["IdArtist"],
+                Name = (string)reader["Art_Name"],
+                FirstName = (string)reader["Art_FirstName"],
+                BirthDate = (DateTime)reader["Art_BirthDate"]
+            };
+        }
+
+
+        public bool Create(Artist a)
+        {
+            Connection connection = new Connection(_connectionString);
+            string sql = "INSERT INTO Artist (Art_Name, Art_FirstName, Art_BirthDate) VALUES (@name, @fname, @bdate)";
+            Command cmd = new Command(sql, false);
+
+            cmd.AddParameter("name", a.Name);
+            cmd.AddParameter("fname", a.FirstName);
+            cmd.AddParameter("bdate", a.BirthDate);
+
+            return connection.ExecuteNonQuery(cmd) == 1;
+        }
+        public bool Update(Artist a)
+        {
+            Connection connection = new Connection(_connectionString);
+            string sql = "UPDATE Artist SET Art_Name = @name, Art_FirstName = @fname, Art_BirthDate = @bdate WHERE IdArtist = @id";
+            Command cmd = new Command(sql, false);
+
+            cmd.AddParameter("name", a.Name);
+            cmd.AddParameter("fname", a.FirstName);
+            cmd.AddParameter("bdate", a.BirthDate);
+            cmd.AddParameter("id", a.IdArtist);
+
+            return connection.ExecuteNonQuery(cmd) == 1;
+        }
+
+    }
+}
