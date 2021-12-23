912fc78c914fa0ec857455935f2a03d3bf7ba3f5
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-12-23 11:30:01 +0100

    Adding User + Comment entitites to DAL

---
+++ MovieCollectionDAL/Entities/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollectionDAL.Entities
+{
+    public class AppUser
+    {
+        public string IdUser { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ MovieCollectionDAL/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollectionDAL.Entities
+{
+    public class Comment
+    {
+        public int IdComment { get; set; }
+        public string Text { get; set; }
+        public int IdMovie { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string LastModifBy { get; set; }
+        public DateTime LastModifDate { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime DeletionDate { get; set; }
+    }
+}
